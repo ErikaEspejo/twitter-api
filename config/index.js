3bd17cfbc96d67ea57679ac158248eceb98f61e7
Erika Espejo <erikaespejo95@gmail.com>
2021-06-06 16:35:11 -0500

    little changes

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const config = {
   http: {
-    host: process.env.HTTP_HOST || '0.0.0.0',
+    host: process.env.HTTP_HOST || "0.0.0.0",
     port: process.env.PORT || process.env.HTTP_PORT,
   },
   logs: {
@@ -16,6 +16,19 @@ const config = {
     connectionString: process.env.DB_CONNECTION_STRING,
   },
   saltRounds: parseInt(process.env.SALT_ROUNDS, 10),
+  mailer: {
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
+    user: process.env.MAILER_USER,
+    password: process.env.MAILER_PASSWORD,
+  },
+  mail: process.env.MAIL_FROM,
+  twitter: {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessTokenKey: process.env.TWITTER_TOKEN_KEY,
+    accessTokenSecret: process.env.TWITTER_TOKEN_SECRET,
+  },
 };
 
 module.exports = { config };
--- libs/sum.js
+++
@@ -1,2 +0,0 @@
-const sum = (n1, n2) => (n1 + n2);
-module.exports = { sum };
