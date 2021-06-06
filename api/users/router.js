38fa294ba379a1f335e18fbd1e4245245991c7de
Erika Espejo <erikaespejo95@gmail.com>
2021-06-06 16:31:22 -0500

    users router update

--- api/users/router.js
+++ api/users/router.js
@@ -1,37 +1,26 @@
-const express = require('express');
-const {
-  list,
-  create,
-  update,
-  remove,
-  login,
-  logout,
-} = require('./controller');
-const { logger } = require('../middleware/logger');
-const { validateUser, validateLogin } = require('../middleware/validator');
-const { authenticator } = require('../middleware/authenticator');
-const { usersAuthorization } = require('../middleware/authorization');
+const express = require("express");
+const { list, create, update, remove, login, logout } = require("./controller");
+const { logger } = require("../middleware/logger");
+const { validateUser, validateLogin } = require("../middleware/validator");
+const { authenticator } = require("../middleware/authenticator");
+const { usersAuthorization } = require("../middleware/authorization");
 
 const router = express.Router();
 
 router.use(logger);
 
 router
-  .route('/')
+  .route("/")
   .get(list)
   .delete(authenticator, usersAuthorization, remove)
   .post(validateUser, create);
 
-router
-  .route('/login')
-  .post(validateLogin, login);
+router.route("/login").post(validateLogin, login);
+
+router.route("/logout").get(logout);
 
 router
-  .route('/logout')
-  .get(logout);
-
-router
-  .route('/:id') //
-  .put(authenticator, usersAuthorization, validateUser, update); // update
+  .route("/:id") //
+  .put(authenticator, usersAuthorization, validateUser, update);
 
 module.exports = router;
